fbf99558deb0afc83a3a0c70f0f73dc5a9a649d9
Sinayel <yanis.louvel402@gmail.com>
2026-01-08 23:43:31 +0100

    testing attack and enemies

---
+++ script/enemie.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var max_speed: float = 25.0
+@export var min_dir_time: float = 1.2
+@export var max_dir_time: float = 2.5
+@export var smooth_factor: float = 4.0
+
+var current_dir: Vector2 = Vector2.ZERO
+var time_left: float = 0.0
+
+func _ready() -> void:
+	randomize()
+	_pick_new_direction()
+
+func _physics_process(delta: float) -> void:
+	time_left -= delta
+	if time_left <= 0.0:
+		_pick_new_direction()
+
+	var target_velocity := current_dir * max_speed
+
+	velocity = velocity.lerp(target_velocity, smooth_factor * delta)
+
+	# Déplacement avec collisions
+	move_and_slide()
+
+	if velocity.length() < 1.0:
+		# Anim idle
+		if anim.sprite_frames and anim.sprite_frames.has_animation("walk_idle"):
+			if !anim.is_playing() or anim.animation != "walk_idle":
+				anim.play("walk_idle")
+		else:
+			if anim.is_playing():
+				anim.stop()
+		return
+
+	if velocity.x > 0.0:
+		anim.flip_h = false
+	elif velocity.x < 0.0:
+		anim.flip_h = true
+
+	if !anim.is_playing() or anim.animation != "walk":
+		anim.play("walk")
+
+func take_damage():
+	queue_free()
+
+func _pick_new_direction() -> void:
+	var r := randi_range(0, 2)
+	if r == 0:
+		current_dir = Vector2.ZERO
+	else:
+		var dir_index := randi_range(0, 7)
+		current_dir = _get_some_vector(dir_index).normalized()
+
+	time_left = randf_range(min_dir_time, max_dir_time)
+
+func _get_some_vector(nb_ale: int) -> Vector2:
+	match nb_ale:
+		0, 7:
+			return Vector2(0, -1)
+		2, 3:
+			return Vector2(0, 1)
+		1, 5:
+			return Vector2(1, 0)
+		4, 6:
+			return Vector2(-1, 0)
+	return Vector2(0, 1)
---
+++ script/newplayer.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var attack_area: Area2D = $AttackPivot/AttackArea
+@onready var attack_pivot: Node2D = $AttackPivot
+
+const speed := 60.0
+
+var is_attacking := false
+var last_dir: Vector2 = Vector2.RIGHT
+
+func _ready() -> void:
+	anim.animation_finished.connect(_on_anim_finished)
+	attack_area.monitoring = false
+	attack_area.connect("body_entered", _on_attack_area_body_entered)
+
+func _physics_process(_delta: float) -> void:
+	# Bloc attaque (prioritaire)
+	if is_attacking:
+		return
+
+	if Input.is_action_just_pressed("click"):
+		is_attacking = true
+		anim.play("attack")
+		anim_player.play("attack")
+		attack_area.monitoring = true
+		return
+
+	# Mouvement
+	var dir := Input.get_vector("left", "right", "up", "down")
+	if dir != Vector2.ZERO:
+		last_dir = dir
+		velocity = speed * dir
+		move_and_slide()
+
+		# Flip du pivot au lieu de flip du sprite
+		if dir.x > 0:
+			anim.flip_h = false
+			attack_pivot.scale.x = 1
+		elif dir.x < 0:
+			anim.flip_h = true
+			attack_pivot.scale.x = -1
+
+
+		if !anim.is_playing() or anim.animation != 'walk':
+			anim.play('walk')
+	else:
+		if anim.sprite_frames and anim.sprite_frames.has_animation('walk_idle'):
+			if !anim.is_playing() or anim.animation != 'walk_idle':
+				anim.play('walk_idle')
+		else:
+			if anim.is_playing():
+				anim.stop()
+
+func _on_anim_finished() -> void:
+	if anim.animation == "attack":
+		is_attacking = false
+		attack_area.monitoring = false
+
+func _on_attack_area_body_entered(body: Node) -> void:
+	if is_attacking and body.is_in_group("enemies"):
+		print("Touche :", body.name)
+		body.take_damage()
+
+		
