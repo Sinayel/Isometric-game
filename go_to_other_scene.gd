39fbf3032329a0db17c60f24ca19ca192e1371aa
Sinayel <113111425+Sinayel@users.noreply.github.com>
2026-01-11 23:43:32 +0100

    changed scene

---
+++ go_to_other_scene.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export_file("*.tscn") var target_scene: String
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Newplayer":
+		get_tree().change_scene_to_file(target_scene)
